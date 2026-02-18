216a67e984e5a3ab850afc100e65e5de7755d225
TouhruCreative <touhru.shott@gmail.com>
2026-02-18 19:30:08 +0500

    update view

--- backend/backendapi/views.py
+++ backend/backendapi/views.py
@@ -1,17 +1,21 @@
-from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from .models import YoutubeVideo
 from .serializers import YoutubeVideoSerializer
 
-class YoutubeVideoView(APIView):
-    def get(self, request):
-        videos = YoutubeVideo.objects.all()
-        serializer = YoutubeVideoSerializer(videos, many=True)
-        return Response(serializer.data)
+# class YoutubeVideoView(APIView):
+#     def get(self, request):
+#         videos = YoutubeVideo.objects.all()
+#         serializer = YoutubeVideoSerializer(videos, many=True)
+#         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = YoutubeVideoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+#     def post(self, request):
+#         serializer = YoutubeVideoSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
+
+class YoutubeVideoViewSet(ModelViewSet):
+    queryset = YoutubeVideo.objects.all()
+    serializer_class = YoutubeVideoSerializer
